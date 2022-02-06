433288fca85a184744f553656a73013a5b7503c4
Roman Zemerov <romanzemerov@gmail.com>
2022-02-06 18:51:14 +0300

    add render post with id from URL

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './tutorial-5.1/App';
+import App from './tutorial-6/App';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
